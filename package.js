c3e1743bbdc2d82a755643eaa7a17f971b063310
Eddie <138073250+Rebel1316@users.noreply.github.com>
2024-01-06 19:36:09 -0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,9 @@
+// Go in terminal and type npm init and type following
+
+package.js
+enter // press enter
+enter // press enter
+enter // press enter
+enter // press enter
+author type in your discord name //mine ex: Rebel
+yes
